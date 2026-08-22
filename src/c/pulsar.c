1e96a8d207748723e5eb0eff108196b081256b6f
Travers McInerney <account@github.trav.sh>
2026-08-22 16:27:08 -0700

    fix(c): replace array address check with tuple length check to satisfy GCC -Werror

--- src/c/pulsar.c
+++ src/c/pulsar.c
@@ -746,7 +746,7 @@ static int tuple_to_int(Tuple *tuple, int default_val) {
       if (tuple->length == 4) return (int)tuple->value->int32;
       return (int)tuple->value->int32;
     case TUPLE_CSTRING:
-      if (tuple->value && tuple->value->cstring) {
+      if (tuple->length > 0) {
         return atoi(tuple->value->cstring);
       }
       break;
@@ -763,7 +763,7 @@ static bool tuple_to_bool(Tuple *tuple, bool default_val) {
     case TUPLE_UINT:
       return tuple_to_int(tuple, default_val ? 1 : 0) != 0;
     case TUPLE_CSTRING:
-      if (tuple->value && tuple->value->cstring) {
+      if (tuple->length > 0) {
         return (strcmp(tuple->value->cstring, "true") == 0 ||
                 strcmp(tuple->value->cstring, "1") == 0);
       }
